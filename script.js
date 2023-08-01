ffe85a0329634670feaaec72597a07b2948a8b7c
Surafels <ssurafel4@gmail.com>
2023-08-01 11:48:26 +0300

    Add speakers as objects

--- script.js
+++ script.js
@@ -11,3 +11,35 @@ menuContainer.classList.remove('hidden');
 }
 closingMenu.addEventListener('click',closeMenu);
 mobileList.addEventListener('click',closeMenu);
+
+
+const speakers=[
+    {
+    id:1,
+    speakerName:'Yochai Benkler',
+    jobTitle:'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    image:'',
+    description:'speaker-e">Benkler studies commons-based peer production, and published his seminal book,The Wealth of Networks in 2006',
+    },
+    {
+        id:2,
+        speakerName:'Yochai Benkler',
+    jobTitle:'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    image:'',
+    description:'speaker-e">Benkler studies commons-based peer production, and published his seminal book,The Wealth of Networks in 2006',
+    },
+    {
+        id:3,
+    speakerName:'Yochai Benkler',
+    jobTitle:'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    image:'',
+    description:'speaker-e">Benkler studies commons-based peer production, and published his seminal book,The Wealth of Networks in 2006',
+    },
+    {
+        id:4,
+    speakerName:'Yochai Benkler',
+    jobTitle:'Berkman Professor of Enterpreneurial Legal Studies at Harvard Law School',
+    image:'',
+    description:'speaker-e">Benkler studies commons-based peer production, and published his seminal book,The Wealth of Networks in 2006',
+    }
+];
