d44a3742adf1370702de4b862e6f1a1d67423323
Surafels <ssurafel4@gmail.com>
2023-08-02 21:39:53 +0300

    Update styles for header

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const humbergerMenu= document.querySelector('#humberger');
-const closingMenu= document.querySelector('.closing');
-const menuContainer= document.querySelector('#mobile-menu')
+const hamburgerMenu= document.querySelector('#hamburger');
+const closingMenu= document.querySelector('#closing-icon');
+const menuContainer= document.querySelector('.menu-mob')
 const mobileList= document.querySelector('#hum-list');
 function openMenu(){
     menuContainer.classList.add('hidden');
 }
-humbergerMenu.addEventListener('click',openMenu);
 function closeMenu(){
 menuContainer.classList.remove('hidden');    
 }
+hamburgerMenu.addEventListener('click',openMenu);
 closingMenu.addEventListener('click',closeMenu);
 mobileList.addEventListener('click',closeMenu);
 
